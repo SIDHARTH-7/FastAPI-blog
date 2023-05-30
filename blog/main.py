221a8766ccc44db804011549cf73c62e425fa2e3
Sidharth Mishra <106550602+SIDHARTH-7@users.noreply.github.com>
2023-05-30 12:20:14 +0530

    30may 23

--- blog/main.py
+++ blog/main.py
@@ -8,7 +8,7 @@ from database import engine,SessionLocal
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)#.......
 
 def get_db():
     db=SessionLocal()
